a46a11ed9afbae0d37533ff554fa3cb222bc6462
Serafeim Vasileiou <vasileiouserafeim@gmail.com>
2020-09-28 13:17:38 +0300

    almost ready

--- src/main/java/gr/uniwa/apice/ApIceApplication.java
+++ src/main/java/gr/uniwa/apice/ApIceApplication.java
@@ -10,18 +10,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApIceApplication /*implements CommandLineRunner*/ {
-   /* @Autowired
-    StudentService studentService;*/
+public class ApIceApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(ApIceApplication.class, args);
     }
 
-    /*@Override
-    public void run(String... args) throws Exception {
-
-        studentService.addStudent(new Student("Serafeim","Vasileiou","cs171072","serafeimv","password","ROLE_ADMIN"));
-
-    }*/
 }
