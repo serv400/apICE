0f3c45a8d5b4b9fd409ed019508dfe686f394fd8
Serafeim Vasileiou <vasileiouserafeim@gmail.com>
2020-09-29 14:41:40 +0300

    postgres update

--- src/main/java/gr/uniwa/apice/ApIceApplication.java
+++ src/main/java/gr/uniwa/apice/ApIceApplication.java
@@ -1,17 +1,13 @@
 package gr.uniwa.apice;
-
 import gr.uniwa.apice.Domain.Student;
-import gr.uniwa.apice.Repository.StudentRepo;
 import gr.uniwa.apice.Service.StudentService;
-import gr.uniwa.apice.Service.StudentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApIceApplication  {
-
+public class ApIceApplication{
     public static void main(String[] args) {
         SpringApplication.run(ApIceApplication.class, args);
     }
